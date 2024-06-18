29210e196b23026a7d2bd5e18e470d594593f6c1
Sankar Nayak <95521439+SankarNayak@users.noreply.github.com>
2024-06-18 19:27:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-
+const bodyParser = require("body-parser");
 const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
     console.log(`${req.method} request for '${req.url}'`);
@@ -15,7 +18,6 @@ app.get('/about', (req, res) => {
     res.send('This is the About Page!');
 });
 
-const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
